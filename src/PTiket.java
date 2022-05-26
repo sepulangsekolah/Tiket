ebaf414afbb3f4a67a2aaf7016fdbcab31092936
laymoth <laymoth@proton.me>
2022-05-26 15:24:40 +0700

    Initial

---
+++ src/PTiket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alinuxrc
+ */
+public class PTiket {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tiket Bioskop = new Tiket();
+        Bioskop.setVisible(true);
+    }
+}
